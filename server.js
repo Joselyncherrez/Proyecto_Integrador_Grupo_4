ccfa46270409987a85e662f9608762c4820c37aa
camicodes <cammila002@hotmail.com>
2022-01-28 13:21:46 -0500

    filtros ordenados

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ const client = new Client({
     user: 'postgres',
     host: 'localhost',
     database: 'Relacional_Autos',
-    password: 'Admin*1234',
+    password: '12345',
     port: 5432,
 });
 
